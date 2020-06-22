70c14f1d922c8f09a0160d0e96107a6ebe9029d7
japoorv <japoorv@LAPTOP-I1U4045A.localdomain>
2020-06-22 16:40:33 +0530

    Added some conditional checks (making more robust)

--- search.rkt
+++ search.rkt
@@ -2,7 +2,7 @@
 (require data/queue)
 ;; Code is heavily inspired from the code listing (search.lisp) of the Building Problem Solver by Forbus & Kleer (http://www.qrg.northwestern.edu/BPS/readme.html)
 #|
-The below code basically provides an interface form making CPS (Problem Solver)
+The below code basically provides an interface for making CPS (Problem Solver)
 which searches the whole problem space (from initial state) using some operators.
 The interface is minimalistic in the sense that very less assumptions have been made in order to generalize it.
 The idea and terms used are all from "Building Problem Solver" book. 
@@ -24,14 +24,16 @@ The idea and terms used are all from "Building Problem Solver" book.
              pr ;; problem whose path has been made pointed to the appropriate problem struct 
              ) #:mutable #:transparent)
 (provide bsolve)
-(define (bsolve pr initial_state)
-  (let ([Qu (make-queue)])
+(define (bsolve pr initial_state);; Solver function explores the problem space for possible paths to a goal 
+  (let ([Qu (make-queue)]) ;; Qu is the queue for bfs
     (enqueue! Qu (path initial_state '() pr))
     (define (bfs)
-      (let ([Qfront (dequeue! Qu)])
-        (cond
-          [((problem-goal-recognizer pr) (path-current-state Qfront)) Qfront]
-          [else (enqueue_list Qu (extend-path Qfront)) (bfs)]
+      (when (non-empty-queue? Qu)
+        (let ([Qfront (dequeue! Qu)])
+          (cond
+            [((problem-goal-recognizer pr) (path-current-state Qfront)) Qfront]
+            [else (enqueue_list Qu (extend-path Qfront)) (bfs)]
+            )
           )
         )
       )
